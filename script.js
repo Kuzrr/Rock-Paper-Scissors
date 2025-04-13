988c545eb52b6d638a911edb5db01254126e6a47
Kuzrr <ptrk7331@gmail.com>
2025-04-13 22:29:01 +0200

    js logic implemented and is working correctly

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 
 function getComputerChoice(){
     let number = Math.floor(Math.random() * 3) + 1;
@@ -14,16 +11,13 @@ function getComputerChoice(){
     }
 }
 
-function getPlayerChoice(choice){
-    switch(choice){
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        case 3:
-            return "scissors";
-    }
-}
+
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+
+playerScore.textContent = 0;
+computerScore.textContent = 0;
+
 
 function playRound(weapon){
     const playerChoiceText = document.querySelector("#playerChoice");
@@ -44,15 +38,27 @@ function playRound(weapon){
     (playerChoice == "paper" && computerChoice == "rock")){
 
         winner.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
-        humanScore++;
+        playerScore.textContent++;
     }else{
         winner.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
-        computerScore++;
+        computerScore.textContent++;
     }
-
+    
+    if (computerScore.textContent == 5) {
+        alert("You lost!");
+        playerScore.textContent = 0;
+        computerScore.textContent = 0;
+        winner.textContent = "";
+    } else if(playerScore.textContent == 5){
+        alert("You won!");
+        playerScore.textContent = 0;
+        computerScore.textContent = 0;
+        winner.textContent = "";
+    }
     
     return 0;
 }
+
 
 const btnRck = document.querySelector("#rock");
 const btnPpr = document.querySelector("#paper");
